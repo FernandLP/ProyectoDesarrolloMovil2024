5077c1fd1c3edaaa73627e6d136b23ee259283d3
Eduardo Lozada <eduardolozada801@gmail.com>
2024-05-08 21:43:01 -0600

    Implementacion de la base de datos

--- app/src/main/java/com/fei/apartados/EmployeeNewAppartView.kt
+++ app/src/main/java/com/fei/apartados/EmployeeNewAppartView.kt
@@ -1,6 +1,9 @@
 package com.fei.apartados
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -13,23 +16,43 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class EmployeeNewAppartView : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
-    private lateinit var linearLayout: LinearLayout
     private lateinit var addButton: Button
     private lateinit var saveButton: Button
     private lateinit var backButton: ImageView
 
+    private lateinit var clientEditText: EditText
+    private lateinit var phoneEditText: EditText
+    private lateinit var infoEditText: EditText
+
+    private lateinit var idEditText: EditText
+    private lateinit var dateTextView: TextView
+    private lateinit var linearLayout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employee_new_appart_view)
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+        val actual = dateFormat.format(calendar.time)
 
-        linearLayout = findViewById(R.id.linearLayoutList)
         addButton = findViewById(R.id.btnAdd)
         saveButton = findViewById(R.id.btnSave)
         backButton = findViewById(R.id.imageViewBack)
+
+        dateTextView = findViewById(R.id.textViewDate)
+        linearLayout = findViewById(R.id.linearLayoutList)
+        idEditText =  findViewById(R.id.editTextID)
+
+        searchLastId()
+
+        dateTextView.text = actual
 
 
         addButton.setOnClickListener {
@@ -47,6 +70,42 @@ class EmployeeNewAppartView : AppCompatActivity() {
         }
 
     }
+
+    @SuppressLint("Range", "ResourceType")
+    private fun searchLastId() {
+        val dbHelper = DatabaseHelper(this)
+        val db = dbHelper.readableDatabase
+
+        // Verificar si la tabla Articulo existe en la base de datos
+        val cursor = db.rawQuery("SELECT * FROM sqlite_master WHERE type='table' AND name='Articulo'", null)
+        if (cursor.count == 0) {
+            // La tabla Articulo no existe, no hacer nada
+            cursor.close()
+            db.close()
+            dbHelper.close()
+            Toast.makeText(this, "No existe la base de datos", Toast.LENGTH_SHORT).show()
+            idEditText.setText("null")
+            return
+        }
+        cursor.close()
+
+        // La tabla Articulo existe, realizar la consulta para obtener el último ID
+        val query = "SELECT MAX(id_apartado) AS lastId FROM Articulo"
+        val idCursor = db.rawQuery(query, null)
+        if (idCursor.moveToFirst()) {
+            val lastId = idCursor.getInt(idCursor.getColumnIndex("lastId"))
+            // Imprimir el último ID
+            Toast.makeText(this, "La base de datos existe y el ultimo valor es $lastId", Toast.LENGTH_SHORT).show()
+            Log.d("Last ID", "El último ID de la tabla Articulo es: $lastId")
+            val num = lastId + 1
+            idEditText.setText(num.toString())
+        }
+        idCursor.close()
+        db.close()
+        dbHelper.close()
+    }
+
+
 
     private fun addItem() {
         val customWidget = LayoutInflater.from(this).inflate(R.layout.widget_new_appart_item, null) // Crear instancia del widget personalizado
